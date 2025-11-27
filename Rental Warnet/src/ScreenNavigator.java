2d29a562b5f867ea918febc813a29d498a2827c2
bang-dyy <ardindidy@gmail.com>
2025-11-27 19:13:33 +0800

    didy

--- Rental Warnet/src/ScreenNavigator.java
+++ Rental Warnet/src/ScreenNavigator.java
@@ -1,10 +1,10 @@
 public interface ScreenNavigator {
 
     void goHome();
-
+    void goToLogin();
+    void goToRegister();
     void goToPCSelection(String paketName);
-
     void goToPayment(String paketName, int pcIndex, long durMillis);
-
     void showCodeThenBack(String paketName, int pcIndex, long durMillis);
+    
 }
--- Rental Warnet/src/WarnetFrame.java
+++ Rental Warnet/src/WarnetFrame.java
@@ -12,16 +12,54 @@ public class WarnetFrame extends JFrame implements ScreenNavigator {
     private PaymentPanel paymentPage;
     private CodePanel codePage;
     private Map<String, Long> paketDurasi;
+    private RegisterPanel registerPage; 
+    private LoginPanel loginPage;
 
-    public WarnetFrame() {
-    }
+   public WarnetFrame() {
+    super("SISTEM BILLING WARNET");
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setSize(800, 600);
+    
+    cards = new CardLayout();
+    root = new JPanel(cards); 
+
+    loginPage = new LoginPanel(this);
+    registerPage = new RegisterPanel(this);
+    homePanel = new JPanel(); 
+    pcSelectionPage = new PCSelectionPanel(this);
+    paymentPage = new PaymentPanel(this);
+    codePage = new CodePanel(this);
+
+    root.add(loginPage, "LOGIN");
+    root.add(registerPage, "REGISTER");
+    root.add(homePanel, "HOME");
+    root.add(pcSelectionPage, "PC_SELECT");
+    root.add(paymentPage, "PAYMENT");
+    root.add(codePage, "CODE");
+
+    add(root);
+    cards.show(root, "LOGIN");
+    setVisible(true);
+}
+
 
     @Override
     public void goHome() {
+         cards.show(root, "HOME");
     }
 
     @Override
     public void goToPCSelection(String paketName) {
+    }
+
+     @Override
+    public void goToLogin() {
+        cards.show(root, "LOGIN");
+    }
+
+    @Override
+    public void goToRegister() {
+        cards.show(root, "REGISTER");
     }
 
     @Override
